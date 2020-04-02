3ed163cec69e766821eaa1a01890513792ba0076
k-usuha <kouta528bado@yahoo.co.jp>
2020-04-02 20:03:56 +0900

    add help search functions

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,5 @@
-# from discord.ext import commands
-import discord
+from discord.ext import commands
+# import discord
 import os
 import traceback
 import re
@@ -23,5 +23,32 @@ async def ping(ctx):
 async def neko(ctx):
     await ctx.send('にゃーん:cat:')
 
+@bot.command()
+async def search(ctx, arg): 
+    for msg in await ctx.pins():
+        if re.search(arg, msg.content):
+            await channel.send(msg.content)
+
+@bot.command()
+async def help(ctx):
+    template = """
+    おいらはマイクラで便利に使えそうなボットだぜ
+    追加して欲しい機能とかあったら言ってくれれば気分で追加するだぜ
+
+    :::: 命令一覧 ::::
+    /neko
+            にゃーん
+
+    /search (検索文字列)
+            例: /search ネザー 
+                    ピン留めされたメッセージの中から、
+                    検索文字列を含むメッセージを表示する。
+                    :warning:   /search   と   (検索文字列)   との間の半角空白に注意
+    
+    /pin
+            これをメッセージのどこかにつけてメッセージを送ると、
+            自動的にピン留めされる。
+    """
+    await ctx.send(template)
 
 bot.run(token)
