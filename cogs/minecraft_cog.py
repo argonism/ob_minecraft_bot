fe9824a3bb858f0e31d293a6fc9e8ab977dbf292
k-usuha <kouta528bado@yahoo.co.jp>
2020-04-03 02:50:44 +0900

    separate bot and cog

---
+++ cogs/minecraft_cog.py
@@ -0,0 +1,57 @@
+﻿from discord.ext import commands
+import os
+import re
+import random
+
+class MinecraftCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.remove_command('help')
+
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send('pong')
+
+    @commands.command()
+    async def neko(self, ctx):
+        await ctx.send('にゃーん:cat:')
+
+    @commands.command()
+    async def search(self, ctx, arg): 
+        for msg in await ctx.pins():
+            if re.search(arg, msg.content):
+                await ctx.send(msg.content)
+
+    @commands.command()
+    async def pin(self, ctx):
+        new_text = ctx.message.content.replace("/pin ", "")
+        new_msg = await ctx.send(new_text)
+        await new_msg.pin()
+
+    @commands.command()
+    async def help(self, ctx):
+        template = """
+        おいらはマイクラで便利に使えそうなボットだぜ
+        追加して欲しい機能とかあったら言ってくれれば気分で追加するだぜ
+
+        :::: 命令一覧 ::::
+        /ping   応答確認
+        /neko   にゃーん
+
+        /search (検索文字列)
+                例: /search ネザー 
+                ピン留めされたメッセージの中から、
+                検索文字列を含むメッセージを表示する。
+                :warning:   /search   と   (検索文字列)   との間の半角空白に注意
+
+        /pin
+                例; /pin -2251/150/-35  経験値トラップ
+                これをメッセージの先頭につけてメッセージを送ると、
+                先頭の /pin を消したメッセージが新たに投稿され、
+                そのメッセージが自動的にピン留めされる。
+        """
+        await ctx.send(template)
+
+def setup(bot):
+    bot.add_cog(MinecraftCog(bot))
--- discordbot.py
+++ discordbot.py
@@ -1,65 +1,40 @@
 from discord.ext import commands
-# import discord
-import os
 import traceback
-import re
-import random
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-bot.remove_command('help')
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+# 読み込むコグの名前を格納しておく。
+INITIAL_EXTENSIONS = [
+    'cogs.minecraft_cog'
+]
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+# クラスの定義。ClientのサブクラスであるBotクラスを継承。
+class MinecraftBot(commands.Bot):
 
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん:cat:')
+    # MyBotのコンストラクタ。
+    def __init__(self, command_prefix):
+        # スーパークラスのコンストラクタに値を渡して実行。
+        super().__init__(command_prefix)
 
-@bot.command()
-async def search(ctx, arg): 
-    for msg in await ctx.pins():
-        if re.search(arg, msg.content):
-            await ctx.send(msg.content)
+        # INITIAL_COGSに格納されている名前から、コグを読み込む。
+        # エラーが発生した場合は、エラー内容を表示。
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
 
-@bot.command()
-async def pin(ctx):
-    new_text = ctx.message.content.replace("/pin ", "")
-    new_msg = await ctx.send(new_text)
-    await new_msg.pin()
+    async def on_command_error(self, ctx, error):
+        orig_error = getattr(error, "original", error)
+        error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+        await ctx.send(error_msg)
 
-@bot.command()
-async def help(ctx):
-    template = """
-    おいらはマイクラで便利に使えそうなボットだぜ
-    追加して欲しい機能とかあったら言ってくれれば気分で追加するだぜ
+    async def on_ready(self):
+        print('-----')
+        print(self.user.name)
+        print([user.name for user in self.users])
+        print('-----')
 
-    :::: 命令一覧 ::::
-    /ping   応答確認
-    /neko   にゃーん
-
-    /search (検索文字列)
-            例: /search ネザー 
-            ピン留めされたメッセージの中から、
-            検索文字列を含むメッセージを表示する。
-            :warning:   /search   と   (検索文字列)   との間の半角空白に注意
-
-    /pin
-            例; /pin -2251/150/-35  経験値トラップ
-            これをメッセージの先頭につけてメッセージを送ると、
-            先頭の /pin を消したメッセージが新たに投稿され、
-            そのメッセージが自動的にピン留めされる。
-    """
-    await ctx.send(template)
-
-# @client.event
-# async def on_message(message):
-
-bot.run(token)
+if __name__ == '__main__':
+    bot = MinecraftBot(command_prefix='/')
+    bot.run(token)
