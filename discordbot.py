7d2d6d86d61a60b6bcb4d740fd4afaa4849dcd43
k-usuha <kouta528bado@yahoo.co.jp>
2020-04-02 20:53:20 +0900

    fix functions

--- discordbot.py
+++ discordbot.py
@@ -7,7 +7,7 @@ import random
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-
+bot.remove_command('help')
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -27,7 +27,11 @@ async def neko(ctx):
 async def search(ctx, arg): 
     for msg in await ctx.pins():
         if re.search(arg, msg.content):
-            await channel.send(msg.content)
+            await ctx.send(msg.content)
+
+@bot.command()
+async def pin(ctx):
+    await ctx.message.pin()
 
 @bot.command()
 async def help(ctx):
@@ -36,19 +40,24 @@ async def help(ctx):
     追加して欲しい機能とかあったら言ってくれれば気分で追加するだぜ
 
     :::: 命令一覧 ::::
-    /neko
-            にゃーん
+    /ping   応答確認
+    /neko   にゃーん
 
     /search (検索文字列)
             例: /search ネザー 
-                    ピン留めされたメッセージの中から、
-                    検索文字列を含むメッセージを表示する。
-                    :warning:   /search   と   (検索文字列)   との間の半角空白に注意
+            ピン留めされたメッセージの中から、
+            検索文字列を含むメッセージを表示する。
+            :warning:   /search   と   (検索文字列)   との間の半角空白に注意
     
     /pin
-            これをメッセージのどこかにつけてメッセージを送ると、
+            例; /pin -2251/150/-35  経験値トラップ
+            これをメッセージの先頭につけてメッセージを送ると、
             自動的にピン留めされる。
     """
     await ctx.send(template)
 
+# @client.event
+# async def on_message(message):
+
+# bot.run(TOKEN)
 bot.run(token)
